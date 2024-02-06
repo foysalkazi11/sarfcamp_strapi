9a7a3d5803bde2c65355e7f8cd761a92cd9d243f
foysalkazi11 <foysalkazi11@gmail.com>
2024-02-06 22:37:53 +0600

    WIP details of article

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,57 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface BlogArticleHeadline extends Schema.Component {
+  collectionName: 'components_blog_article_headlines';
+  info: {
+    displayName: 'headline';
+    description: '';
+  };
+  attributes: {
+    headline: Attribute.String & Attribute.Required;
+    slug: Attribute.String & Attribute.Required;
+  };
+}
+
+export interface BlogArticleLandscapeImage extends Schema.Component {
+  collectionName: 'components_blog_article_landscape_images';
+  info: {
+    displayName: 'landscapeImage';
+  };
+  attributes: {
+    image: Attribute.Media & Attribute.Required;
+    ImageCaption: Attribute.String;
+  };
+}
+
+export interface BlogArticleParagraphWithImage extends Schema.Component {
+  collectionName: 'components_blog_article_paragraph_with_images';
+  info: {
+    displayName: 'paragraphWithImage';
+  };
+  attributes: {
+    paragraph: Attribute.RichText & Attribute.Required;
+    image: Attribute.Media & Attribute.Required;
+    isLandscape: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<true>;
+    imageShowRight: Attribute.Boolean &
+      Attribute.Required &
+      Attribute.DefaultTo<true>;
+    imageCaption: Attribute.String;
+  };
+}
+
+export interface BlogArticleParagraph extends Schema.Component {
+  collectionName: 'components_blog_article_paragraphs';
+  info: {
+    displayName: 'paragraph';
+    description: '';
+  };
+  attributes: {
+    Paragraph: Attribute.RichText & Attribute.Required;
+  };
+}
+
 export interface InfoBlockButton extends Schema.Component {
   collectionName: 'components_info_block_buttons';
   info: {
@@ -17,6 +69,10 @@ export interface InfoBlockButton extends Schema.Component {
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'blog-article.headline': BlogArticleHeadline;
+      'blog-article.landscape-image': BlogArticleLandscapeImage;
+      'blog-article.paragraph-with-image': BlogArticleParagraphWithImage;
+      'blog-article.paragraph': BlogArticleParagraph;
       'info-block.button': InfoBlockButton;
     }
   }
